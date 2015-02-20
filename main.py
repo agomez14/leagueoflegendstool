c26ddd5957722d1c3f0e915e6d663b819a5c4549
agomez14 <Chivas@Armandos-MacBook-Pro.local>
2015-02-20 00:57:42 -0600

    API calls for summoner's and match history

--- main.py
+++ main.py
@@ -16,13 +16,41 @@
 #
 import webapp2, jinja2, os, logging, urllib2, urllib, json
 
+from google.appengine.api import urlfetch
+from settings import *
+
 jinja_environment = jinja2.Environment(loader=
-    jinja2.FileSystemLoader(os.path.dirname(__file__)))
+    jinja2.FileSystemLoader("html"))
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        template_values = {}
+        template = jinja_environment.get_template('main.html')
+        self.response.out.write(template.render(template_values))
+
+class SumdIdHandler(webapp2.RequestHandler):
+    def get(self):
+        summoner_name = str(self.request.get('summoner')).lower()
+        region = self.request.get('region').lower()
+        
+        # Summoner Name to Summoner ID
+        sum_url = "https://na.api.pvp.net/api/lol/"+region+"/v1.4/summoner/by-name/"+summoner_name+"?api_key="+RIOT_API_KEY
+        response = urlfetch.fetch(url=sum_url)
+        sum_data = json.loads(response.content)
+
+        #SummonerID to MatchHistory
+        sum_id = str(sum_data[summoner_name]['id'])
+        hist_url = "https://na.api.pvp.net/api/lol/"+region+"/v2.2/matchhistory/"+sum_id+"?api_key="+RIOT_API_KEY
+        response = urlfetch.fetch(url=hist_url)
+        hist_data = json.loads(response.content)
+        
+        template_values = {}
+        for x in range(0,len(hist_data['matches'])):
+            template_values[x] = hist_data['matches'][x]
+        template = jinja_environment.get_template('history.html')
+        self.response.out.write(template.render(template_values=template_values))
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/sumid', SumdIdHandler)
 ], debug=True)
